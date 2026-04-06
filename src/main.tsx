b14cb2d06da37d5b4a5b196b3ea3ae3e53d47e79
shahidmuneer17 <shahidmuneer17@gmail.com>
2026-04-06 15:53:43 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import './styles/index.css'
+import { APP_NAME } from './config'
+
+document.title = APP_NAME;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
